5ef8d7b4724363a584b317b60806a96ef29b590e
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-08 10:37:56 +0800

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -1,14 +1,9 @@
 import Home from "../components/Home";
 
 export default function home() {
-  const handleClick=()=>{
-    localStorage.clear();
-    window.location.reload();
-}
     return (
       <div>
         <Home />
-        <button onClick={handleClick}>Logout</button>
       </div> 
     );
 }
