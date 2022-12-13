eaeb1e10a74f32b5fe982c8ef3df8153baf1912c
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-13 10:27:58 +0800

    Update Story3.jsx

--- components/Story3.jsx
+++ components/Story3.jsx
@@ -9,7 +9,7 @@ export default function Story() {
         <div className={styles.container}>
             <div class={styles.cards}>
             <div class={styles.card}>
-                <image class={styles.card__image} src="3.png"/>
+                <image class={styles.card__image} src="3.png" alt="image"/>
                 <div class={styles.card__content}>
                     <p>            Globe & MetaverseGo</p>
                     <p>            By: Val Jose Mendoza </p>
