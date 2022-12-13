00af1ab716b97e1f59398c1077548b214a391d94
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-13 10:27:43 +0800

    Update Story2.jsx

--- components/Story2.jsx
+++ components/Story2.jsx
@@ -9,7 +9,7 @@ export default function Story() {
         <div className={styles.container}>
             <div class={styles.cards}>
             <div class={styles.card}>
-                <image class={styles.card__image} src="2.png"/>
+                <image class={styles.card__image} src="2.png" alt="image"/>
                 <div class={styles.card__content}>
                     <p>          HOW TO OVERCOME YOUR</p>
                     <p>               FEAR OF FAILURE </p>
