08da1ab946ae5f67960223b6ef30b3732e1a1f50
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 11:39:50 +0800

    Update Story.jsx

--- components/Story.jsx
+++ components/Story.jsx
@@ -6,7 +6,7 @@
 import styles from "./Story.module.css";
 
 export default function Story() {
-    return (
+  
         <div className={styles.container}>
             <div class={styles.cards}>
             <div class={styles.card}>
