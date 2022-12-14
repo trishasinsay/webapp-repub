082cf8b481829388a01275d8569feab8ee74d3b6
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 14:05:35 +0800

    Update Story6.jsx

--- components/Story6.jsx
+++ components/Story6.jsx
@@ -83,11 +83,11 @@ export default function Story() {
         </div>
           <div className={styles.link}>
             <button>
-              <link href="/Titles">Go back to Content </link>
+              <a href="/Titles">Go back to Content </a>
             </button>
             </div>
             <div className={styles.back_button}>
-            <button><link href="/HOW_TO_DEAL_WITH_TOXIC_PEOPLE">Back</link></button>
+            <button><a href="/HOW_TO_DEAL_WITH_TOXIC_PEOPLE">Back</a></button>
             </div>
             </div>    
           </div>
