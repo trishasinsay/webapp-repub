d955e81497de576b3047146bda2b63bc5797bed2
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 15:46:08 +0800

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -19,7 +19,7 @@ export default function Main() {
         or specialized.
         </p>
         <div className={styles.GetStarted}>
-          <link href="/Login"> Get Started</link>
+          <link href="/Login"> Get Started/>
         </div>
       </div>
     </div>
