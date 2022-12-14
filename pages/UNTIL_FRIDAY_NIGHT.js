3e802a879f85f6a3b6a289c339ec160b48d81f13
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 17:40:49 +0800

    Update UNTIL_FRIDAY_NIGHT.js

--- pages/UNTIL_FRIDAY_NIGHT.js
+++ pages/UNTIL_FRIDAY_NIGHT.js
@@ -1,5 +1,5 @@
-import Navbar1 from "../pages/Navbar1";
-import Story from "../pages/Story";
+import Navbar1 from "../components/Navbar1";
+import Story from "../components/Story";
 
 export default function UFN() {
   return (
