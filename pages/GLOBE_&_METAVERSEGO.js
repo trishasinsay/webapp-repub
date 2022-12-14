e826fee63c044e96a312fa3945e7c473278d9426
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 17:16:45 +0800

    Update GLOBE_&_METAVERSEGO.js

--- pages/GLOBE_&_METAVERSEGO.js
+++ pages/GLOBE_&_METAVERSEGO.js
@@ -1,5 +1,5 @@
-import Story3 from "../pages/Story3";
-import Navbar1 from "../pages/Navbar1";
+import Story3 from "../pages/components/Story3";
+import Navbar1 from "../pages/components/Navbar1";
 
 export default function globe() {
     return (
