e63195077e8e3d044f72ae8b6ec1d6d559924506
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 10:17:43 +0800

    Update Story.jsx

--- components/Story.jsx
+++ components/Story.jsx
@@ -1,6 +1,6 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,300&display=swap" rel="stylesheet">
 </link>
 
 import styles from "./Story.module.css";
