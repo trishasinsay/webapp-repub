2c59c93c99ef6412b5fbbe0f5970b8780bc579b3
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 14:16:56 +0800

    Update Story.jsx

--- components/Story.jsx
+++ components/Story.jsx
@@ -32,7 +32,7 @@ export default function Story() {
             </div>
             <div className={styles.link}>
               <button>
-                <a href="/HOW_TO_OVERCOME_YOUR_FEAR_OF_FAILURE">Next </a>
+                <link href="/HOW_TO_OVERCOME_YOUR_FEAR_OF_FAILURE">Next </link>
               </button>
               </div>
           </div>
