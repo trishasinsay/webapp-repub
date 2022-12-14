be1096c670838a56ab36e7d736b93f672c09b155
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 15:55:59 +0800

    Update Upload.jsx

--- components/Upload.jsx
+++ components/Upload.jsx
@@ -1,4 +1,4 @@
-<link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.googleapis.com"></link>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
 
 
