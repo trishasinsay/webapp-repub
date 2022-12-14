bd68200bf20b1392e2c716e26b096976b5b203af
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 10:31:23 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,17 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link href="/fonts/fonts_style.css" rel="stylesheet"/>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+  render() {
+    return (
+        <Html>
+            <Head>
+                <link href="/fonts/fonts_style.css" rel="stylesheet"/>
+            </Head>
+        </Html>
+    )
 }
 
 
