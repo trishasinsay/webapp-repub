704ac53043bcee17c4df9dea2bca5556c6e87f3d
trishasinsay <119267063+trishasinsay@users.noreply.github.com>
2022-12-14 10:24:02 +0800

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,46 +1,17 @@
-// pages/_document.js
-
-import Document, { Html, Head, Main, Home, Content, Login, Register, Navbar, Navbar1 } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet"></link>
-          </link>
-      
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet"></link>
-          </link>
-      
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Gruppo&display=swap" rel="stylesheet"></link>
-          </link>
-      
-          <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-          </link>
-          
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-          <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet"></link>
-          </link>
-      
-          <link rel="preconnect" href="https://fonts.googleapis.com">
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
-          </link>
-      
-        </Head>
-        <body>
+        <link href="/fonts/fonts_style.css" rel="stylesheet"/>
+      </Head>
+      <body>
         <Main />
-        <Home/>
-        <Content/>
-        <Login/>
-        <Register/>
-        <Navbar/>
-        <Navbar1/>
-        </body>
-      </Html>
-    )
-  }
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
+
 
